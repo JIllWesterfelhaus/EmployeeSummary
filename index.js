a765699bc27825002c87ec80a9105c897f15712f
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-12 13:01:50 -0500

    added commas to index.js

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ function init () {
             type: "input",
             message: "Would you like to add a new employee to the roster?",
             name:  "newEmployee"
-        }
+        },
         {
             type: "input",
             message: "What is the employee's name?",
             name: "name"
-        }
+        },
         {
             type: "input",
             message: "What is the employee's ID number?",
             name: "idNum"
-        }
+        },
         {
             type: "input",
             message: "What is the employee's email?",
             name: "email"
-        }
+        },
 
         //if statement? ask if employee is manager, engineer or intern
         //if manager
@@ -30,13 +30,13 @@ function init () {
             type: "input",
             message:  "What is the manager's office number?",
             name: "officeNum"
-        }
+        },
         //if engineer
         {
             type: "input",
             message:  "What is the engineer's GitHub user name?",
             name: "gitHub"
-        }
+        },
         //if intern
         {
             type: "input",
